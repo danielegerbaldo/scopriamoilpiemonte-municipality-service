a169c034cbba2ff631c0f50730bbe08d87d62c39
danielegerbaldo <daniele.gerbaldo@gmail.com>
2021-05-22 18:44:00 +0200

    Eureka registration added

--- src/main/java/TAASS/ServiceDBComuni/ServiceDbComuniApplication.java
+++ src/main/java/TAASS/ServiceDBComuni/ServiceDbComuniApplication.java
@@ -2,7 +2,9 @@ package TAASS.ServiceDBComuni;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ServiceDbComuniApplication {
 
