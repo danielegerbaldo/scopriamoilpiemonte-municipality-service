a96aa1e91e141930f26f9bc778ac807dfed774fe
Flavio Roman <flavioroman@MacBook-Pro.station>
2021-07-12 20:57:31 +0200

    Added new tests

--- src/test/java/ContractRestClientApplicationTest.java
+++ src/test/java/ContractRestClientApplicationTest.java
@@ -14,21 +14,103 @@ public class ContractRestClientApplicationTest {
             .downloadStub("com.example", "contract-rest-service", "0.0.1-SNAPSHOT", "stubs")
             .stubsMode(StubRunnerProperties.StubsMode.LOCAL);
 
+    //Testing - TORINO
     @Test
     public void check_comune_nome() {
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<Comune> personResponseEntity = restTemplate.getForEntity("http://localhost/api/v1/comune/info-comune/1006",Comune.class);
+        ResponseEntity<Comune> personResponseEntity = restTemplate.getForEntity("http://localhost/api/v1/comune/info-comune/1272",Comune.class);
         Assertions.assertThat(personResponseEntity).isNotNull();
         BDDAssertions.then(personResponseEntity.getStatusCodeValue()).isEqualTo(200);
-        BDDAssertions.then(personResponseEntity.getBody().getNome()).isEqualTo("Almese");
+        BDDAssertions.then(personResponseEntity.getBody().getNome()).isEqualTo("Torino");
     }
-
     @Test
     public void check_comune_provincia() {
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<Comune> personResponseEntity = restTemplate.getForEntity("http://localhost/api/v1/comune/info-comune/1006",Comune.class);
+        ResponseEntity<Comune> personResponseEntity = restTemplate.getForEntity("http://localhost/api/v1/comune/info-comune/1272",Comune.class);
         Assertions.assertThat(personResponseEntity).isNotNull();
         BDDAssertions.then(personResponseEntity.getStatusCodeValue()).isEqualTo(200);
         BDDAssertions.then(personResponseEntity.getBody().getProvincia()).isEqualTo("TO");
+    }
+    @Test
+    public void check_comune_regione() {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Comune> personResponseEntity = restTemplate.getForEntity("http://localhost/api/v1/comune/info-comune/1272",Comune.class);
+        Assertions.assertThat(personResponseEntity).isNotNull();
+        BDDAssertions.then(personResponseEntity.getStatusCodeValue()).isEqualTo(200);
+        BDDAssertions.then(personResponseEntity.getBody().getRegione()).isEqualTo("Piemonte");
+    }
+    @Test
+    public void check_comune_cap() {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Comune> personResponseEntity = restTemplate.getForEntity("http://localhost/api/v1/comune/info-comune/1272",Comune.class);
+        Assertions.assertThat(personResponseEntity).isNotNull();
+        BDDAssertions.then(personResponseEntity.getStatusCodeValue()).isEqualTo(200);
+        BDDAssertions.then(personResponseEntity.getBody().getCAP()).isEqualTo("10121-10156");
+    }
+    @Test
+    public void check_comune_lat() {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Comune> personResponseEntity = restTemplate.getForEntity("http://localhost/api/v1/comune/info-comune/1272",Comune.class);
+        Assertions.assertThat(personResponseEntity).isNotNull();
+        BDDAssertions.then(personResponseEntity.getStatusCodeValue()).isEqualTo(200);
+        BDDAssertions.then(personResponseEntity.getBody().getLat()).isEqualTo(45.0732745);
+    }
+    @Test
+    public void check_comune_lng() {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Comune> personResponseEntity = restTemplate.getForEntity("http://localhost/api/v1/comune/info-comune/1272",Comune.class);
+        Assertions.assertThat(personResponseEntity).isNotNull();
+        BDDAssertions.then(personResponseEntity.getStatusCodeValue()).isEqualTo(200);
+        BDDAssertions.then(personResponseEntity.getBody().getLng()).isEqualTo(7.68068748);
+    }
+
+    //Testing - ROMA
+    @Test
+    public void check_comune_nome_1() {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Comune> personResponseEntity = restTemplate.getForEntity("http://localhost/api/v1/comune/info-comune/58091",Comune.class);
+        Assertions.assertThat(personResponseEntity).isNotNull();
+        BDDAssertions.then(personResponseEntity.getStatusCodeValue()).isEqualTo(200);
+        BDDAssertions.then(personResponseEntity.getBody().getNome()).isEqualTo("Roma");
+    }
+    @Test
+    public void check_comune_provincia_1() {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Comune> personResponseEntity = restTemplate.getForEntity("http://localhost/api/v1/comune/info-comune/58091",Comune.class);
+        Assertions.assertThat(personResponseEntity).isNotNull();
+        BDDAssertions.then(personResponseEntity.getStatusCodeValue()).isEqualTo(200);
+        BDDAssertions.then(personResponseEntity.getBody().getProvincia()).isEqualTo("RM");
+    }
+    @Test
+    public void check_comune_regione_1() {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Comune> personResponseEntity = restTemplate.getForEntity("http://localhost/api/v1/comune/info-comune/58091",Comune.class);
+        Assertions.assertThat(personResponseEntity).isNotNull();
+        BDDAssertions.then(personResponseEntity.getStatusCodeValue()).isEqualTo(200);
+        BDDAssertions.then(personResponseEntity.getBody().getRegione()).isEqualTo("Lazio");
+    }
+    @Test
+    public void check_comune_cap_1() {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Comune> personResponseEntity = restTemplate.getForEntity("http://localhost/api/v1/comune/info-comune/58091",Comune.class);
+        Assertions.assertThat(personResponseEntity).isNotNull();
+        BDDAssertions.then(personResponseEntity.getStatusCodeValue()).isEqualTo(200);
+        BDDAssertions.then(personResponseEntity.getBody().getCAP()).isEqualTo("00118-00199");
+    }
+    @Test
+    public void check_comune_lat_1() {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Comune> personResponseEntity = restTemplate.getForEntity("http://localhost/api/v1/comune/info-comune/58091",Comune.class);
+        Assertions.assertThat(personResponseEntity).isNotNull();
+        BDDAssertions.then(personResponseEntity.getStatusCodeValue()).isEqualTo(200);
+        BDDAssertions.then(personResponseEntity.getBody().getLat()).isEqualTo(41.89277044);
+    }
+    @Test
+    public void check_comune_lng_1() {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Comune> personResponseEntity = restTemplate.getForEntity("http://localhost/api/v1/comune/info-comune/58091",Comune.class);
+        Assertions.assertThat(personResponseEntity).isNotNull();
+        BDDAssertions.then(personResponseEntity.getStatusCodeValue()).isEqualTo(200);
+        BDDAssertions.then(personResponseEntity.getBody().getLng()).isEqualTo(12.48366723);
     }
 }
